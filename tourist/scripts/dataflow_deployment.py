06821bcc993ab6f75e8d00fb75d82d86829dd9d2
Tom Brown <tomgith4@thecap.org>
2022-09-28 10:05:32 -0700

    change prefect deployment to run flow in docker container. works in dev, untested on production.

--- tourist/scripts/dataflow_deployment.py
+++ tourist/scripts/dataflow_deployment.py
@@ -1,28 +1,44 @@
 import pathlib
 
+from prefect.infrastructure import DockerContainer
+
 import tourist.scripts.dataflow
 import tourist.config
 from prefect.deployments import Deployment
-from prefect.filesystems import LocalFileSystem
 
 dev_data_path = pathlib.Path(__file__).parent.parent.parent / "dev-data"
 
-run_gb_deployment_dev_data = Deployment.build_from_flow(
+Deployment.build_from_flow(
     flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
     name="run_gb_fetch_and_sync_dev_data",
 
     infra_overrides={"env.DATA_DIR": str(dev_data_path),
                      "env.FLASK_ENV": "development"},
     work_queue_name="test",
-)
-run_gb_deployment_dev_data.apply()
+).apply()
 
 
-run_gb_deployment_production = Deployment.build_from_flow(
+container = DockerContainer(image='tomgobravo/tourist-with-flask:tourist-docker')
+Deployment.build_from_flow(
+    flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
+    name="run_gb_fetch_and_sync_dockerhub_development",
+    infrastructure=container,
+    infra_overrides={"env.DATA_DIR": "/data",
+                     "env.FLASK_ENV": "development",
+                     "volumes": ['/home/thecap/code/tourist-with-flask/dev-data:/data']},
+    work_queue_name="development",
+    path="/app",
+).apply()
+
+
+container = DockerContainer(image='tomgobravo/tourist-with-flask:tourist-docker')
+Deployment.build_from_flow(
     flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
     name="run_gb_fetch_and_sync_production",
-    infra_overrides={"env.FLASK_ENV": "production"},
-    work_queue_name="production",
-    storage=LocalFileSystem(basepath="/var/local/www-data/tourist-prefect-agent/deployment/run_gb_fetch_and_sync_production"),
-)
-run_gb_deployment_production.apply()
+    infrastructure=container,
+    infra_overrides={"env.DATA_DIR": "/data",
+                     "env.FLASK_ENV": "production",
+                     "volumes": ['/var/local/www-data:/data']},
+    work_queue_name="development",
+    path="/app",
+).apply()
