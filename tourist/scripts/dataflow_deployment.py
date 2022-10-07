1b312288c9ff5272675103a9100a52892142ab97
Tom Brown <tomgith4@thecap.org>
2022-10-06 18:22:07 -0700

    add prefect agent running in docker, seems to work in dev, untested in prod.

--- tourist/scripts/dataflow_deployment.py
+++ tourist/scripts/dataflow_deployment.py
@@ -1,44 +1,24 @@
 import pathlib
 
-from prefect.infrastructure import DockerContainer
-
 import tourist.scripts.dataflow
 import tourist.config
 from prefect.deployments import Deployment
 
-dev_data_path = pathlib.Path(__file__).parent.parent.parent / "dev-data"
 
 Deployment.build_from_flow(
     flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
     name="run_gb_fetch_and_sync_dev_data",
-
-    infra_overrides={"env.DATA_DIR": str(dev_data_path),
-                     "env.FLASK_ENV": "development"},
-    work_queue_name="test",
-).apply()
-
-
-container = DockerContainer(image='tomgobravo/tourist-with-flask:tourist-docker')
-Deployment.build_from_flow(
-    flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
-    name="run_gb_fetch_and_sync_dockerhub_development",
-    infrastructure=container,
     infra_overrides={"env.DATA_DIR": "/data",
-                     "env.FLASK_ENV": "development",
-                     "volumes": ['/home/thecap/code/tourist-with-flask/dev-data:/data']},
+                     "env.FLASK_ENV": "development"},
     work_queue_name="development",
-    path="/app",
 ).apply()
 
 
-container = DockerContainer(image='tomgobravo/tourist-with-flask:tourist-docker')
 Deployment.build_from_flow(
     flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
     name="run_gb_fetch_and_sync_production",
-    infrastructure=container,
     infra_overrides={"env.DATA_DIR": "/data",
-                     "env.FLASK_ENV": "production",
-                     "volumes": ['/var/local/www-data:/data']},
-    work_queue_name="development",
-    path="/app",
+                     "env.FLASK_ENV": "production"},
+    work_queue_name="production",
+    storage=None,
 ).apply()
