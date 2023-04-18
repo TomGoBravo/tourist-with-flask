6519815e2b7794cc974fc1900aa3dc966f1fc123
Tom Brown <tomgith4@thecap.org>
2023-04-17 16:30:05 -1000

    fix setup-dev-data.sh (#58)
    flask in setup-dev-data.sh was accessing the production path /data instead of development.

--- .devcontainer/setup-dev-data.sh
+++ .devcontainer/setup-dev-data.sh
@@ -7,4 +7,4 @@ mkdir logs
 cp tourist/tests/spatial_metadata.sqlite dev-data/tourist.db
 
 # Import JSON lines file. Get from old system or in the test directory.
-FLASK_APP=tourist flask sync import_jsonl tourist/tests/testentities.jsonl
+FLASK_APP=tourist TOURIST_ENV=development flask --debug sync import_jsonl tourist/tests/testentities.jsonl
