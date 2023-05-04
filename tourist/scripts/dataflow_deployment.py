f3cba98a649b5f9f5bc0acc6690537bbe8692f5e
Tom Brown <tomgith4@thecap.org>
2023-05-03 14:36:34 -1000

    only deploy one prefect flow, preparing for having a dev prefect server

--- tourist/scripts/dataflow_deployment.py
+++ tourist/scripts/dataflow_deployment.py
@@ -7,19 +7,7 @@ from prefect.deployments import Deployment
 
 Deployment.build_from_flow(
     flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
-    name="run_gb_fetch_and_sync_dev_data",
-    infra_overrides={"env.DATA_DIR": "/data",
-                     "env.TOURIST_ENV": "development"},
+    name="run_gb_fetch_and_sync",
     work_queue_name="development",
-    path='/app',
-).apply()
-
-
-Deployment.build_from_flow(
-    flow=tourist.scripts.dataflow.run_gb_fetch_and_sync,
-    name="run_gb_fetch_and_sync_production",
-    infra_overrides={"env.DATA_DIR": "/data",
-                     "env.TOURIST_ENV": "production"},
-    work_queue_name="production",
-    path='/app',
+    path='/workspace/tourist-with-flask',
 ).apply()
